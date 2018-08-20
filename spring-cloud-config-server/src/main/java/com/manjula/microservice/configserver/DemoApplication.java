a5648ca0c14aad3e1988c561c76f7a73a3f1c5cd
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-08-20 09:39:06 +0530

    add configuration server details

--- spring-cloud-config-server/src/main/java/com/manjula/microservice/configserver/DemoApplication.java
+++ spring-cloud-config-server/src/main/java/com/manjula/microservice/configserver/DemoApplication.java
@@ -2,7 +2,9 @@ package com.manjula.microservice.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class DemoApplication {
 
