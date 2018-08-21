753c876f447777c19e95df1b9fb87361a4741f7b
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-08-21 09:07:54 +0530

    implement currency exchange service

---
+++ currency-exchange-service/src/main/java/com/manjula/microservices/currencyexchange/ExchangeValue.java
@@ -0,0 +1,25 @@
+package com.manjula.microservices.currencyexchange;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class ExchangeValue {
+
+    @Id
+    private Long id;
+    @Column(name="currency_from")
+    private String from;
+    @Column(name="currency_to")
+    private String to;
+    private BigDecimal conversionMultiple;
+    private int port;
+
+}
---
+++ currency-exchange-service/src/main/java/com/manjula/microservices/currencyexchange/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.manjula.microservices.currencyexchange;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+
+    ExchangeValue findByFromAndTo(String from, String to);
+
+}
