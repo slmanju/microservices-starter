a9c96e0035e2cd5368ad065710eda2ffa4698242
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-08-22 09:20:31 +0530

    fetch data using feign

--- currency-conversion-service/src/main/java/com/manjula/microservices/currencyconversion/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/manjula/microservices/currencyconversion/CurrencyExchangeServiceProxy.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 //@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/from/{from}/to/{to}")
     CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
