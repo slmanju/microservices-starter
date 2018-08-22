6eadd14c5b1dd44cb9174f6d5da127a259b0d225
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-08-22 09:22:45 +0530

    client side load balance with ribbon

--- currency-conversion-service/src/main/java/com/manjula/microservices/currencyconversion/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/manjula/microservices/currencyconversion/CurrencyExchangeServiceProxy.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
-//@FeignClient(name = "currency-exchange-service")
-//@RibbonClient(name = "currency-exchange-service")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/from/{from}/to/{to}")
